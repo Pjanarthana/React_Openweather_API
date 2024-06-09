6710525747d1737b401d119a551aae8286030174
Pjanarthana <janarthananoffl@gmail.com>
2024-06-09 23:22:16 +0530

    completed

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "17aba9e1f2msh2fc9329fddc747cp1f3f53jsn6bfe70c71234",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9f3cd907b26f3460491046e997bb626b";
